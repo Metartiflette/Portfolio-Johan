3a5144c9b4491c6e50071974335a6796bc54358c
Tom Boullay <tom@famille-boullay.fr>
2025-10-26 18:17:41 +0100

    update de favicon et metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,25 +5,30 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/global/Navbar";
 import Footer from "./components/global/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Johan Vuillerme - Portfolio",
-  description: "A personal portfolio site built with Sanity and Next.js",
+  description:
+    "Portfolio de Johan Vuillerme, réalisateur et monteur vidéo freelance. Courts-métrages, projets clients, collaborations, drone, etc... Basé à Chambéry (France), disponible partout !",
+  icons: {
+    icon: "/favicon.ico",
+  },
   metadataBase: new URL("https://johanvuillerme.fr/"),
-  // openGraph: {
-  //   images: "add-your-open-graph-image-url-here",
-  // },
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       <body className={`${inter.className} text-white bg-black`}>
-          <Navbar />
-          {children}
-          <Footer />
+        <Navbar />
+        {children}
+        <Footer />
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
