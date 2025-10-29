bb9bd2daed84c0c28f7fa1d7d61932c02e88a23c
Metartiflette <lla.vuillerme.johan@gmail.com>
2025-10-29 21:59:08 +0100

    Update IntroOverlay.tsx

--- app/components/IntroOverlay.tsx
+++ app/components/IntroOverlay.tsx
@@ -48,7 +48,7 @@ export default function IntroOverlay({ intro, onFinish }: IntroOverlayProps) {
 
             setTimeout(() => onFinish(), 2000);
 1
-            setTimeout(() => setPhase("fade-out"), 1500);
+            setTimeout(() => setPhase("fade-out"), 1800);
         } else {
             setPhase("fade-out");
             setTimeout(onFinish, 800);
